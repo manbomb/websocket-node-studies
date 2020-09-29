9694d9cddb1b1775ea873a9c5145fc3ab333910c
Sérgio Lucas Avilla da Silva <sergio@aiqfome.com>
2020-09-29 16:41:43 -0300

    enviar mensagem depois da conexão

--- client.js
+++ client.js
@@ -7,3 +7,12 @@ var token = jwt.sign({
 }, '0cc175b9c0f1b6a831c399e269772661');
 
 client.connect(`ws://localhost:8080/?token=${token}`);
+
+client.on('connect', function(connection) {
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+    connection.sendUTF("mandei");
+})
